6cea9b6e9e950c1433aa26ff733410f39a6c7fef
Eduardo Araiza <tech@eduardoaraiza.com>
2024-02-16 22:23:04 -0800

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,38 +6,139 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (username && password) {
+      if (!doesExist(username)) { 
+        users.push({"username":username,"password":password});
+        return res.status(200).json({message: "User successfully registered. Now you can login"});
+      } else {
+        return res.status(404).json({message: "User already exists!"});    
+      }
+    } 
+    return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Get all books
+  res.send(JSON.stringify({books},null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const book = books[isbn]
+    res.send(JSON.stringify(book,null,4));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let author = req.params.author
+    let result = {
+        "booksByAuthor": []
+    }
+    let keys = Object.keys(books)
+    for(let key of keys) {
+        let book = books[key]
+        if(book.author == author) {
+            result['booksByAuthor'].push(book)
+        }
+    }
+    res.send(result)
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let title = req.params.title
+    let result = {
+      "booksByTitle": []
+    }
+    let keys = Object.keys(books)
+    for(let key of keys) {
+        let book = books[key]
+        if(book.title == title) {
+            result['booksByTitle'].push(book)
+        }
+    }
+    res.send(result)
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let isbn = req.params.isbn
+    let review = books[isbn].reviews
+    res.send(JSON.stringify(review,null,5));
+});
+
+public_users.get("/books", function(req,res) {
+    let promise = new Promise( function(resolve,reject) {
+        resolve(res.send(JSON.stringify(books, null, 4)));
+    });
+
+    promise.then( () => console.log("Book list has been sent successfully"));
+});
+  
+public_users.get('/isbn/:isbn',function (req, res) {
+    let isbn = req.params.isbn;
+    let book = books[isbn];
+    let promise = new Promise( function(resolve, reject) {
+        if(book) {
+        resolve(res.send( JSON.stringify(book, null, 5) ));
+        }
+        else {
+        resolve(res.send("Sorry, We couldn't find a book with this ISBN"));
+        }
+    });
+
+    promise.then( () => console.log("The details of the book with the requested ISBN has been sent successfully ") );
+
+});
+
+public_users.get('/author/:author',function (req, res) {
+    let promise = new Promise( function(resolve,reject) {
+        let author = req.params.author;
+        let result = {
+        "booksByAuthor": []
+        }
+        
+        let keys = Object.keys(books);
+
+        for(let key of keys) {
+          let book = books[key];
+          if(book.author == author) {
+              result['booksByAuthor'].push(book)
+          }
+        }
+        
+        resolve(res.send( JSON.stringify(result,null,5) ))
+    });
+
+    promise.then( () => console.log("The list of books with the requested Author has been sent successfully") )
+});
+
+public_users.get('/title/:title',function (req, res) {
+    let promise = new Promise( function(resolve,reject){
+        let title = req.params.title;
+        
+        let result = {
+          "booksByTitle": []
+        }
+        
+        let keys = Object.keys(books);
+
+        for(let key of keys) {  
+          let book = books[key]
+          if(book.title == title) {
+              result['booksByTitle'].push(book)
+          }
+        }
+
+        resolve(res.send( JSON.stringify(result,null,5) ));
+    });
+
+    promise.then( () => console.log("Books with the requested title has been sent successfully.") )
 });
 
 module.exports.general = public_users;
